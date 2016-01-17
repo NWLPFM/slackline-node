01bb38f7c2e1e84309ac77815ab2d971a90fc024
Finn <finn@finn.io>
2016-01-16 17:09:53 -0800

    Fix semiconons to make jshint happy

--- settings.js
+++ settings.js
@@ -4,16 +4,13 @@ var settings = {};
 settings.tokens = {
 	'[FIRST SLACK INSTANCE]' : '[FIRST API KEY]',
 	'[SECOND SLACK INSTANCE]' : '[SECOND API KEY]',
-}
+};
 
 settings.domainReferrer = {
 	'[FIRST SLACK INSTANCE]' : '[SECOND SLACK INSTANCE]',
 	'[SECOND SLACK INSTANCE]' : '[FIRST SLACK INSTANCE]'
-}
+};
 
 settings.postUrl = '/services/hooks/incoming-webhook?token=';
 
 module.exports = settings;
-
-
-
